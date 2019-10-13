417eac8e02b8633813f7a72870d7270ee3c0ae4e
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-10-13 13:40:48 +0300

    Also added constants.py file

---
+++ src/forests/content/constants.py
@@ -0,0 +1,45 @@
+RESOURCE_TYPES = [
+    ('RASTER_DATA', 'Raster data'),
+    ('TABULAR_DATA', 'Tabular data'),
+    ('MAP', 'Map'),
+    ('VISUALIZATION', 'Visualization'),
+    ('DOCUMENTATION', 'Documentation'),
+    ('REPORT', 'Report',)
+]
+
+
+DATA_SOURCES = [
+    ('Sample based', [
+        "NFI", "State of Europe's Forests", "Forest Resource Assessment"
+    ]),
+    ('Raster based',
+        ["Forest Map / HRL Forest", "Corine Land Cover", "Global Forest Watch",
+         "Maltese Planning Authority"]
+     ),
+]
+
+NUTS_LEVELS = [
+    ('nuts_level0', 'Level 0'),
+    ('nuts_level1', 'Level 1'),
+    ('nuts_level2', 'Level 2'),
+    ('nuts_level3', 'Level 3'),
+    ('nuts_level4', 'Level 4'),
+]
+
+DATASETS = ['NFI', 'FAO', 'FRA', 'GFW', 'Alpine Convention', 'SoEF']
+
+GEOCOVERAGE_REGIONS = [
+    'EEA39',
+    'EU28',
+    'FAO234',
+    'SOEF46',
+    # ... TODO: fill in with other countries
+]
+
+ACCESSIBILITY_LEVELS = [
+    ('public', 'Publicly available',),
+    ('registration', 'Download with registration or request'),
+    ('restricted', 'Restricted')
+]
+
+INFO_LEVELS = ['A', 'B', 'C', 'D', 'E']
--- src/forests/content/vocabulary.py
+++ src/forests/content/vocabulary.py
@@ -6,7 +6,8 @@ from plone.app.vocabularies.catalog import KeywordsVocabulary as KV
 from z3c.formwidget.optgroup.widget import OptgroupTerm
 
 from .constants import (ACCESSIBILITY_LEVELS, DATA_SOURCES, DATASETS,
-                        GEOCOVERAGE_REGIONS, NUTS_LEVELS, RESOURCE_TYPES)
+                        GEOCOVERAGE_REGIONS, INFO_LEVELS, NUTS_LEVELS,
+                        RESOURCE_TYPES)
 
 
 @implementer(IVocabularyFactory)
@@ -62,6 +63,9 @@ alsoProvides(geocoverage_vocabulary, IVocabularyFactory)
 
 accessibility_vocabulary = generic_vocabulary(ACCESSIBILITY_LEVELS)
 alsoProvides(accessibility_vocabulary, IVocabularyFactory)
+
+info_levels_vocabulary = generic_vocabulary(INFO_LEVELS)
+alsoProvides(info_levels_vocabulary, IVocabularyFactory)
 
 PublishersVocabularyFactory = KeywordsVocabulary('publisher')
 TopicsVocabularyFactory = KeywordsVocabulary('topics')
