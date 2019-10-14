02c6caee6912b8f23ba4a8de4c74365b726678bc
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-10-14 08:26:50 +0300

    Added plone site serializer override

--- src/forests/content/browser/restapi.py
+++ src/forests/content/browser/restapi.py
@@ -1,8 +1,16 @@
-from zope.component import adapter
+import json
+
+from zope.component import adapter, getMultiAdapter
 from zope.interface import Interface, implementer
 
 from forests.content.interfaces import IBasicDataProvider, IDataProvider
-from plone.restapi.interfaces import IExpandableElement
+# -*- coding: utf-8 -*-
+from plone.restapi.batching import HypermediaBatch
+from plone.restapi.interfaces import (IExpandableElement, ISerializeToJson,
+                                      ISerializeToJsonSummary)
+from plone.restapi.serializer.expansion import expandable_elements
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.interfaces import IPloneSiteRoot
 
 
 @implementer(IExpandableElement)
@@ -24,5 +32,75 @@ class ConnectorData(object):
 
         connector = IDataProvider(self.context)
         result['connector-data']["data"] = connector.provided_data
+
+        return result
+
+
+@implementer(ISerializeToJson)
+@adapter(IPloneSiteRoot, Interface)
+class SerializeSiteRootToJson(object):
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def _build_query(self):
+        path = "/".join(self.context.getPhysicalPath())
+        query = {
+            "path": {"depth": 1, "query": path},
+            "sort_on": "getObjPositionInParent",
+        }
+
+        return query
+
+    def __call__(self, version=None):
+        version = "current" if version is None else version
+
+        if version != "current":
+            return {}
+
+        query = self._build_query()
+
+        catalog = getToolByName(self.context, "portal_catalog")
+        brains = catalog(query)
+
+        batch = HypermediaBatch(self.request, brains)
+
+        result = {
+            # '@context': 'http://www.w3.org/ns/hydra/context.jsonld',
+            "@id": batch.canonical_url,
+            "id": self.context.id,
+            "@type": "Plone Site",
+            "title": self.context.Title(),
+            "parent": {},
+            "is_folderish": True,
+            "description": self.context.description,
+            "tiles": json.loads(getattr(self.context, "tiles", "{}")),
+            "tiles_layout": json.loads(
+                getattr(self.context, "tiles_layout", "{}")
+            ),  # noqa
+        }
+
+        # this is the place where the code is change from the original.
+        # We want to expose the layout property
+        # The override might not be needed, I think (Interface) in the
+        # descriminator is the browser layer.
+        layout = getattr(self.context, 'layout')
+
+        if layout:
+            result["layout"] = layout
+
+        # Insert expandable elements
+        result.update(expandable_elements(self.context, self.request))
+
+        result["items_total"] = batch.items_total
+
+        if batch.links:
+            result["batching"] = batch.links
+
+        result["items"] = [
+            getMultiAdapter((brain, self.request), ISerializeToJsonSummary)()
+
+            for brain in batch
+        ]
 
         return result
