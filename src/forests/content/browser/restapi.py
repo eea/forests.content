f0009f0afe6da625a9ff2152314eeba79a9e8646
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-11-14 14:29:02 +0200

    Moved provides and site customization to eea.restapi

--- src/forests/content/browser/restapi.py
+++ src/forests/content/browser/restapi.py
@@ -1,21 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import json
 
-from zope.component import adapter, getMultiAdapter
-from zope.interface import Interface, implementer, providedBy
+from zope.component import adapter
+from zope.interface import Interface, implementer
 
 from forests.content.interfaces import IBasicDataProvider, IDataProvider
-from forests.theme.interfaces import IForestsThemeLayer
-from plone.dexterity.interfaces import IDexterityContainer, IDexterityContent
-from plone.restapi.batching import HypermediaBatch
-from plone.restapi.interfaces import (IExpandableElement, ISerializeToJson,
-                                      ISerializeToJsonSummary)
-from plone.restapi.serializer.dxcontent import (SerializeFolderToJson,
-                                                SerializeToJson)
-from plone.restapi.serializer.expansion import expandable_elements
-from Products.CMFCore.utils import getToolByName
-from Products.CMFPlone.interfaces import IPloneSiteRoot
+from plone.restapi.interfaces import IExpandableElement
 
 
 @implementer(IExpandableElement)
@@ -39,97 +29,3 @@ class ConnectorData(object):
         result['connector-data']["data"] = connector.provided_data
 
         return result
-
-
-@implementer(ISerializeToJson)
-@adapter(IPloneSiteRoot, IForestsThemeLayer)
-class SerializeSiteRootToJson(object):
-    def __init__(self, context, request):
-        self.context = context
-        self.request = request
-
-    def _build_query(self):
-        path = "/".join(self.context.getPhysicalPath())
-        query = {
-            "path": {"depth": 1, "query": path},
-            "sort_on": "getObjPositionInParent",
-        }
-
-        return query
-
-    def __call__(self, version=None):
-        version = "current" if version is None else version
-
-        if version != "current":
-            return {}
-
-        query = self._build_query()
-
-        catalog = getToolByName(self.context, "portal_catalog")
-        brains = catalog(query)
-
-        batch = HypermediaBatch(self.request, brains)
-
-        result = {
-            # '@context': 'http://www.w3.org/ns/hydra/context.jsonld',
-            "@id": batch.canonical_url,
-            "id": self.context.id,
-            "@type": "Plone Site",
-            "title": self.context.Title(),
-            "parent": {},
-            "is_folderish": True,
-            "description": self.context.description,
-            "tiles": json.loads(getattr(self.context, "tiles", "{}")),
-            "tiles_layout": json.loads(
-                getattr(self.context, "tiles_layout", "{}")
-            ),  # noqa
-        }
-
-        # this is the place where the code is change from the original.
-        # We want to expose the layout property
-        # The override might not be needed, I think (Interface) in the
-        # descriminator is the browser layer.
-        layout = getattr(self.context, 'layout', None)
-
-        if layout:
-            result["layout"] = layout
-
-        # Insert expandable elements
-        result.update(expandable_elements(self.context, self.request))
-
-        result["items_total"] = batch.items_total
-
-        if batch.links:
-            result["batching"] = batch.links
-
-        result["items"] = [
-            getMultiAdapter((brain, self.request), ISerializeToJsonSummary)()
-
-            for brain in batch
-        ]
-        result['@provides'] = ['{}.{}'.format(I.__module__, I.__name__)
-                               for I in providedBy(self.context)]
-
-        return result
-
-
-@adapter(IDexterityContent, IForestsThemeLayer)
-class DexterityContentSerializer(SerializeToJson):
-    def __call__(self, version=None, include_items=True):
-        res = super(DexterityContentSerializer, self).__call__(version,
-                                                               include_items)
-        res['@provides'] = ['{}.{}'.format(I.__module__, I.__name__)
-                            for I in providedBy(self.context)]
-
-        return res
-
-
-@adapter(IDexterityContainer, IForestsThemeLayer)
-class DexterityContainerSerializer(SerializeFolderToJson):
-    def __call__(self, version=None, include_items=True):
-        res = super(DexterityContainerSerializer, self).__call__(version,
-                                                                 include_items)
-        res['@provides'] = ['{}.{}'.format(I.__module__, I.__name__)
-                            for I in providedBy(self.context)]
-
-        return res
