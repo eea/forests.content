1466c630dfa58f8dd7ecd2a4b6468b490580cb59
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-12-14 08:59:35 +0200

    Also hide the blocks layout schemata

--- src/forests/content/patches.py
+++ src/forests/content/patches.py
@@ -7,16 +7,24 @@ from plone.supermodel import interfaces
 
 @implementer(interfaces.ISchemaPlugin)
 @adapter(IFormFieldProvider)
-class TweakRichTextSchema(object):
+class SchemaTweaks(object):
     """
     """
 
-    order = 10001
+    order = 999999
 
     def __init__(self, schema):
         self.schema = schema
 
     def __call__(self):
+
         if self.schema.getName() == 'IRichTextBehavior':
             field = self.schema['text']
-            field.description = u'Rich text, double click for toolbar'
+            field.description = u'Rich text, double click for toolbar.'
+
+            return
+
+        if self.schema.getName() == 'IBlocks':
+            del self.schema._Element__tagged_values[
+                'plone.supermodel.fieldsets'
+            ]
