dc7490c57ab9e9d0f47be74073fd6f7b0456a6be
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-12-14 08:46:12 +0200

    Added tweak schema code

--- src/forests/content/patches.py
+++ src/forests/content/patches.py
@@ -1,29 +1,22 @@
-# from zope.component import adapter
-# from zope.interface import Interface, implementer
-#
-# from plone.app.contenttypes.behaviors.richtext import IRichText
-# from plone.supermodel import interfaces
-#
-# KEY = 'forests.tweaks.richtext'
-#
-#
-# @implementer(interfaces.ISchemaPlugin)
-# @adapter(IRichText)
-# class TweakRichTextSchema(object):
-#     """
-#     """
-#
-#     order = 1000
-#
-#     def __init__(self, schema):
-#         self.schema = schema
-#
-#     def __call__(self):
-#         pass
-#
-#
-# IRichText.setTaggedValue(KEY,
-#                          [
-#                              (Interface, 'blocks', 'true'),
-#                          ]
-#                          )
+from zope.component import adapter
+from zope.interface import implementer
+
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.supermodel import interfaces
+
+
+@implementer(interfaces.ISchemaPlugin)
+@adapter(IFormFieldProvider)
+class TweakRichTextSchema(object):
+    """
+    """
+
+    order = 10001
+
+    def __init__(self, schema):
+        self.schema = schema
+
+    def __call__(self):
+        if self.schema.getName() == 'IRichTextBehavior':
+            field = self.schema['text']
+            field.description = u'Rich text, double click for toolbar'
